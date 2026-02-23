b55379d4be1c2def7800df02004d813e67f470fe
Kittu G <grewalg@umich.edu>
2026-02-22 20:40:05 -0500

    fixed syntax error

--- queries/query8.sql
+++ queries/query8.sql
@@ -7,7 +7,7 @@ JOIN (
     UNION
     SELECT user2_id AS friend_id FROM project2.Public_Friends WHERE user1_id = userID
 ) f ON u.user_id = f.friend_id
-ORDER BY u.year_of_birth ASC, u.month_of_birth ASC, u.day_of_birth ASC, f.user_id DESC
+ORDER BY u.year_of_birth ASC, u.month_of_birth ASC, u.day_of_birth ASC, f.friend_id DESC
 FETCH FIRST 1 ROWS ONLY;
 --youngest query
 SELECT u.user_id, u.first_name, u.last_name
@@ -17,5 +17,5 @@ JOIN (
     UNION
     SELECT user2_id AS friend_id FROM project2.Public_Friends WHERE user1_id = userID
 ) f ON u.user_id = f.friend_id
-ORDER BY u.year_of_birth DESC, u.month_of_birth DESC, u.day_of_birth DESC, f.user_id DESC
+ORDER BY u.year_of_birth DESC, u.month_of_birth DESC, u.day_of_birth DESC, f.friend_id DESC
 FETCH FIRST 1 ROWS ONLY;
